4b6df87b253b057760276c2ac51a80acc3eecc22
Wout Heijnen <woutheijnen@hotmail.com>
2018-09-26 22:58:05 +0200

    Update battle tracks and some others to crystal version with yellow soundfont

--- audio/music/celadon.asm
+++ audio/music/celadon.asm
@@ -1,115 +1,140 @@
 Music_Celadon_Ch1::
-	tempo 144
+	tempo 146
 	volume 7, 7
-	duty 3
-	toggleperfectpitch
-	notetype 12, 2, 15
-	rest 8
+Music_Celadon_branch_f436c::
+	notetype $c, $a, $6
 	octave 3
-	D_ 8
-
-Music_Celadon_branch_b6d4::
-	duty 2
-	notetype 12, 11, 2
-	G_ 4
-	B_ 4
-	G_ 2
-	B_ 4
-	G_ 2
-	E_ 1
-	E_ 1
-	G_ 1
-	E_ 1
-	B_ 2
-	octave 4
-	C_ 2
-	octave 3
-	A_ 8
-	F# 4
-	A_ 4
-	F# 2
-	A_ 6
+	A_ 7
+	G# 1
 	A_ 1
+	D_ 3
+	E_ 2
+	F# 2
+	callchannel Music_Celadon_branch_f43cc
+Music_Celadon_branch_f437d::
+	D_ 2
+	F# 1
+	A_ 1
+	D_ 1
+	F# 1
+	A_ 1
+	F# 1
+	loopchannel 3, Music_Celadon_branch_f437d
+	D_ 2
+	G_ 1
 	B_ 1
 	octave 4
+	D_ 1
 	C_ 1
 	octave 3
 	B_ 1
-	A_ 2
-	B_ 2
-	G_ 4
-	octave 4
-	G_ 4
-	octave 3
-	G_ 4
-	B_ 4
-	G_ 2
-	B_ 4
-	G_ 2
-	E_ 1
-	F# 1
 	G_ 1
-	A_ 1
-	B_ 2
-	octave 4
-	C_ 2
-	octave 3
-	A_ 8
-	A_ 2
-	B_ 2
-	octave 4
-	C_ 2
-	octave 3
-	B_ 2
-	A_ 2
-	G_ 2
-	F# 2
-	G_ 2
-	F# 4
+	callchannel Music_Celadon_branch_f43cc
+	D_ 1
+	C# 1
 	D_ 1
 	E_ 1
 	F# 1
 	G_ 1
-	A_ 8
-	notetype 12, 9, 4
-	B_ 8
-	G_ 4
-	D_ 4
-	G_ 4
+	A_ 1
+	B_ 1
 	A_ 2
-	octave 4
-	C_ 2
+	G_ 2
+	F# 2
+	E_ 2
+	F# 16
+	callchannel Music_Celadon_branch_f43e1
+	callchannel Music_Celadon_branch_f43fe
+	octave 2
+	B_ 2
+	A# 1
+	B_ 1
 	octave 3
-	B_ 8
-	A_ 8
-	F# 4
-	G_ 4
-	A_ 4
-	G_ 4
-	F# 4
-	A_ 4
-	B_ 8
-	G_ 4
-	D_ 4
-	G_ 4
-	A_ 2
+	D_ 1
+	G_ 1
+	A_ 1
+	G_ 1
+	F# 1
+	G_ 1
+	A_ 1
+	G# 1
+	A_ 1
 	octave 4
-	C_ 2
+	C_ 1
 	octave 3
-	B_ 8
+	B_ 1
+	A_ 1
+	callchannel Music_Celadon_branch_f43e1
+	callchannel Music_Celadon_branch_f43fe
+	D_ 2
+	C# 1
+	D_ 1
+	F# 1
+	G_ 1
+	A_ 1
+	G_ 1
 	A_ 8
-	F# 4
-	G_ 4
-	A_ 4
-	G_ 4
-	F# 4
-	A_ 4
-	loopchannel 0, Music_Celadon_branch_b6d4
+	loopchannel 0, Music_Celadon_branch_f436c
 
+Music_Celadon_branch_f43cc::
+	D_ 2
+	G_ 1
+	B_ 1
+	D_ 1
+	G_ 1
+	B_ 1
+	G_ 1
+	loopchannel 3, Music_Celadon_branch_f43cc
+	D_ 2
+	F# 1
+	A_ 1
+	octave 4
+	D_ 1
+	C# 1
+	D_ 1
+	octave 3
+	A_ 1
+	endchannel
+
+Music_Celadon_branch_f43e1::
+	octave 2
+	G_ 2
+	B_ 1
+	octave 3
+	D_ 1
+	loopchannel 4, Music_Celadon_branch_f43e1
+	C_ 2
+	octave 2
+	B_ 1
+	octave 3
+	C_ 1
+	D_ 1
+	E_ 1
+	F# 1
+	A_ 1
+	G_ 1
+	A_ 1
+	B_ 1
+	octave 4
+	C_ 1
+	D_ 1
+	C# 1
+	D_ 1
+	octave 3
+	B_ 1
+	endchannel
+
+Music_Celadon_branch_f43fe::
+	D_ 2
+	F# 1
+	A_ 1
+	loopchannel 4, Music_Celadon_branch_f43fe
+	endchannel
 
 Music_Celadon_Ch2::
-	duty 3
-	notetype 12, 12, 2
+	vibrato $12, $3, $4
+	duty $2
+	notetype $c, $b, $7
 	octave 4
 	D_ 1
 	C# 1
@@ -119,12 +144,64 @@ Music_Celadon_Ch2::
 	E_ 1
 	F# 1
 	G_ 1
-	notetype 12, 10, 0
 	A_ 8
+Music_Celadon_branch_f441a::
+	callchannel Music_Celadon_branch_f4463
+	A_ 4
+	F# 4
+	A_ 2
+	F# 6
+	A_ 1
+	G_ 1
+	F# 1
+	G_ 1
+	A_ 2
+	B_ 2
+	G_ 4
+	B_ 4
+	callchannel Music_Celadon_branch_f4463
+	A_ 2
+	G_ 2
+	F# 2
+	G_ 2
+	F# 2
+	E_ 2
+	D_ 2
+	C# 2
+	D_ 4
+	D_ 1
+	E_ 1
+	F# 1
+	A_ 1
+	octave 5
+	D_ 6
+	octave 1
+	F# 2
+	callchannel Music_Celadon_branch_f4474
+	octave 1
+	G_ 3
+	B_ 1
+	A_ 2
+	octave 2
+	D_ 2
+	octave 1
+	A_ 6
+	F# 2
+	callchannel Music_Celadon_branch_f4474
+	octave 1
+	A_ 3
+	octave 2
+	D_ 1
+	A_ 4
+	D_ 6
+	octave 4
+	F# 1
+	A_ 1
+	octave 5
+	D_ 16
+	loopchannel 0, Music_Celadon_branch_f441a
 
-Music_Celadon_branch_b74a::
-	duty 2
-	notetype 12, 12, 2
+Music_Celadon_branch_f4463::
 	octave 4
 	B_ 4
 	G_ 4
@@ -139,273 +216,155 @@ Music_Celadon_branch_b74a::
 	octave 4
 	B_ 2
 	A_ 8
-	A_ 4
-	F# 4
-	A_ 2
-	F# 6
-	A_ 1
-	G_ 1
-	F# 1
-	G_ 1
-	A_ 2
-	B_ 2
-	G_ 4
-	B_ 4
-	B_ 4
-	G_ 4
-	B_ 2
-	G_ 4
-	octave 3
-	B_ 2
-	octave 4
-	B_ 1
-	G_ 1
-	A_ 1
-	B_ 1
-	octave 5
-	C_ 2
-	octave 4
-	B_ 2
-	A_ 8
-	A_ 2
-	G_ 2
-	F# 2
-	G_ 2
-	F# 2
-	E_ 2
-	D_ 2
-	C# 2
-	D_ 8
-	octave 5
-	D_ 8
-	notetype 12, 11, 4
-	octave 4
-	D_ 8
-	C_ 2
-	octave 3
-	B_ 2
-	A_ 2
-	B_ 2
-	octave 4
-	E_ 4
-	F# 4
-	D_ 8
-	C_ 8
-	octave 3
-	A_ 2
-	B_ 2
-	octave 4
-	C_ 2
-	D_ 2
-	E_ 2
-	C_ 2
-	D_ 2
-	E_ 2
-	D_ 8
-	D_ 8
-	C_ 2
-	octave 3
-	B_ 2
-	A_ 2
-	B_ 2
-	octave 4
-	E_ 4
-	D_ 2
-	E_ 2
-	D_ 8
-	C_ 8
-	A_ 2
-	G_ 2
-	F# 2
-	E_ 2
-	F# 2
-	E_ 2
-	D_ 2
-	E_ 2
-	D_ 8
-	loopchannel 0, Music_Celadon_branch_b74a
+	endchannel
 
+Music_Celadon_branch_f4474::
+	G_ 3
+	B_ 1
+	octave 2
+	D_ 4
+	octave 1
+	G_ 2
+	B_ 2
+	octave 2
+	D_ 4
+	octave 1
+	G_ 3
+	octave 2
+	C_ 1
+	D_ 4
+	octave 1
+	G_ 2
+	B_ 2
+	octave 2
+	D_ 4
+	octave 1
+	A_ 3
+	octave 2
+	D_ 1
+	F# 4
+	octave 1
+	A_ 2
+	octave 2
+	D_ 2
+	F# 4
+	endchannel
 
 Music_Celadon_Ch3::
-	notetype 12, 1, 3
-	rest 8
-	octave 5
-	D_ 1
-	C# 1
-	D_ 1
-	E_ 1
-	F# 1
-	E_ 1
-	F# 1
-	G_ 1
-
-Music_Celadon_branch_b7c1::
-	octave 4
-	D_ 2
-	B_ 2
-	D_ 2
-	B_ 2
-	D_ 2
-	B_ 2
-	D_ 2
-	B_ 2
-	D_ 2
-	B_ 2
-	D_ 2
-	B_ 2
-	D_ 2
+	vibrato $12, $2, $4
+Music_Celadon_branch_f4495::
+	notetype $c, $2, $5
+	octave 3
 	A_ 2
-	D_ 2
-	A_ 2
-	D_ 2
-	A_ 2
-	D_ 2
-	A_ 2
-	D_ 2
-	A_ 2
-	D_ 2
-	A_ 2
-	D_ 2
-	A_ 2
-	D_ 2
-	A_ 2
-	D_ 2
-	B_ 2
-	D_ 2
-	B_ 2
-	D_ 2
-	B_ 2
-	D_ 2
-	B_ 2
-	D_ 2
-	B_ 2
-	D_ 2
-	B_ 2
-	D_ 2
-	B_ 2
-	D_ 2
-	B_ 2
-	D_ 2
-	A_ 2
-	D_ 2
-	A_ 2
-	D_ 2
-	A_ 2
-	G_ 2
-	A_ 2
-	G_ 2
+	rest 2
 	F# 2
+	rest 2
+	D_ 8
+	callchannel Music_Celadon_branch_f44fd
+Music_Celadon_branch_f44a1::
+	octave 2
+	A_ 2
+	rest 2
+	octave 3
+	D_ 4
+	loopchannel 3, Music_Celadon_branch_f44a1
+	octave 2
+	G_ 1
+	rest 1
+	B_ 1
+	rest 1
+	octave 3
+	D_ 3
+	octave 2
+	F# 1
+	callchannel Music_Celadon_branch_f44fd
+	A_ 1
+	rest 1
+	octave 3
+	D_ 1
+	rest 1
+	octave 3
+	F# 3
+	octave 2
+	G# 1
+	A_ 1
+	rest 1
+	octave 3
+	C# 1
+	rest 1
 	E_ 2
 	C# 2
 	D_ 2
 	rest 6
+	octave 2
+	A_ 2
+	rest 6
+	callchannel Music_Celadon_branch_f4510
+	octave 5
+	E_ 2
+	rest 2
 	F# 2
+	rest 2
+	D_ 4
 	rest 4
-	F# 2
-	G_ 2
-	octave 5
-	D_ 2
+	C_ 8
 	octave 4
-	G_ 2
-	octave 5
-	D_ 2
-	octave 4
-	G_ 2
-	octave 5
-	D_ 2
-	octave 4
-	G_ 2
-	B_ 2
-	G_ 2
-	octave 5
-	E_ 2
-	D_ 2
-	C_ 2
-	D_ 2
-	octave 4
-	B_ 2
-	G_ 2
-	octave 5
-	C_ 2
-	octave 4
-	F# 2
-	octave 5
-	D_ 2
-	octave 4
-	F# 2
-	octave 5
-	D_ 2
-	octave 4
-	F# 2
-	octave 5
-	D_ 2
-	octave 4
-	F# 2
-	octave 5
-	D_ 2
-	octave 4
-	F# 2
-	octave 5
-	E_ 2
-	D_ 2
-	C_ 2
-	D_ 2
-	octave 4
-	B_ 2
 	A_ 2
+	B_ 2
 	octave 5
 	C_ 2
-	octave 4
-	G_ 2
-	octave 5
 	D_ 2
-	octave 4
-	G_ 2
-	octave 5
+	E_ 2
+	rest 2
 	D_ 2
-	octave 4
-	G_ 2
-	octave 5
-	D_ 2
-	octave 4
-	G_ 2
-	B_ 2
-	G_ 2
+	E_ 2
+	D_ 4
+	rest 4
+	callchannel Music_Celadon_branch_f4510
 	octave 5
 	E_ 2
+	rest 2
 	D_ 2
-	C_ 2
-	D_ 2
-	octave 4
-	B_ 2
-	G_ 2
-	octave 5
-	C_ 2
-	octave 4
-	F# 2
-	octave 5
-	D_ 2
-	octave 4
-	F# 2
-	octave 5
-	D_ 2
-	octave 4
-	F# 2
-	octave 5
-	D_ 2
-	octave 4
-	F# 2
-	octave 5
-	D_ 2
-	octave 4
-	F# 2
-	octave 5
-	D_ 2
-	C_ 2
-	octave 4
-	B_ 2
+	E_ 2
+	D_ 4
+	rest 4
+	C_ 8
 	A_ 2
 	G_ 2
 	F# 2
 	E_ 2
-	loopchannel 0, Music_Celadon_branch_b7c1
+	F# 2
+	E_ 2
+	D_ 2
+	E_ 2
+	D_ 2
+	rest 6
+	loopchannel 0, Music_Celadon_branch_f4495
+
+Music_Celadon_branch_f44fd::
+	octave 2
+	G_ 2
+	rest 2
+	octave 3
+	D_ 4
+	loopchannel 3, Music_Celadon_branch_f44fd
+	octave 2
+	A_ 1
+	rest 1
+	octave 3
+	D_ 1
+	rest 1
+	F# 3
+	octave 2
+	G# 1
+	endchannel
+
+Music_Celadon_branch_f4510::
+	octave 5
+	D_ 8
+	C_ 2
+	octave 4
+	B_ 2
+	A_ 2
+	B_ 2
+	endchannel
--- audio/music/routes1.asm
+++ audio/music/routes1.asm
@@ -1,126 +1,295 @@
 Music_Routes1_Ch1::
-	tempo 152
+	tempo 184
 	volume 7, 7
-	vibrato 4, 2, 3
-	duty 2
-	toggleperfectpitch
-
-Music_Routes1_branch_9be9::
-	notetype 12, 10, 1
+	vibrato $16, $1, $1
+	duty $3
+	notetype $c, $8, $3
+	octave 4
+	D_ 1
+	E_ 1
+	F# 2
+	F# 2
+	D_ 2
+	octave 3
+	A_ 2
+	octave 4
+	B_ 2
+	A_ 2
+	G_ 2
+	F# 2
+	D_ 2
+	octave 3
+	A_ 2
+	F# 2
+	G_ 1
+	A_ 1
+	G_ 2
+	E_ 2
+	G_ 1
+	E_ 1
+	octave 4
+	D_ 1
+	E_ 1
+	F# 2
+	F# 2
+	D_ 2
+	octave 3
+	A_ 2
+	octave 4
+	B_ 2
+	A_ 2
+	G_ 3
+	E_ 1
+	E_ 2
+	G_ 2
+	A_ 2
+	G_ 2
+	F# 2
+	octave 5
+	D_ 2
+	D_ 2
+	rest 2
+Music_Routes1_branch_ec04a::
+	rest 2
+	octave 4
+	D_ 2
+	octave 3
+	A_ 2
 	rest 4
 	octave 4
 	D_ 2
-	D_ 6
+	octave 3
+	A_ 2
+	rest 2
+	rest 2
+	octave 4
 	D_ 2
-	D_ 6
+	octave 3
+	A_ 3
+	octave 4
+	D_ 1
+	octave 3
+	E_ 2
+	G_ 2
+	G_ 2
+	rest 2
+	rest 2
+	octave 4
+	C# 2
+	octave 3
+	A_ 2
+	rest 4
+	octave 4
+	C# 2
+	octave 3
+	A_ 2
+	rest 2
+	rest 2
+	octave 4
+	C# 2
+	C# 1
+	octave 3
+	B_ 1
+	A_ 1
+	G_ 1
+	F# 2
+	A_ 2
+	octave 4
 	D_ 2
+	rest 2
+	rest 2
+	octave 3
+	F# 1
+	G_ 1
+	A_ 4
+	rest 2
+	D_ 1
+	E_ 1
+	F# 2
+	F# 1
+	G_ 1
+	A_ 2
+	A_ 1
+	B_ 1
+	A_ 2
+	A_ 1
+	B_ 1
+	A_ 2
+	octave 4
+	G_ 2
+	G_ 2
+	octave 3
+	F# 1
+	E_ 1
+	rest 2
+	E_ 1
+	F# 1
+	G_ 6
+	E_ 1
+	octave 2
+	B_ 1
+	octave 3
+	C# 2
+	E_ 1
+	F# 1
+	G_ 4
+	notetype $8, $8, $3
+	A_ 1
+	B_ 1
+	A_ 1
+	notetype $c, $8, $3
+	G_ 1
+	E_ 1
+	F# 2
+	octave 4
+	A_ 2
+	A_ 2
+	octave 3
+	A_ 1
+	G_ 1
+	F# 4
+	A_ 4
+	G_ 2
+	F# 2
+	E_ 2
+	G_ 2
+	F# 4
+	A_ 3
+	F# 1
+	E_ 2
+	A_ 2
+	octave 4
+	C# 2
+	octave 3
+	A_ 1
+	G_ 1
+	duty $2
+	octave 6
+	F# 4
+	A_ 4
+	G_ 2
+	F# 2
+	E_ 3
+	G_ 1
+	F# 2
+	D_ 2
+	E_ 2
+	C# 2
+	D_ 2
+	octave 5
+	D_ 2
+	D_ 2
+	rest 2
+	loopchannel 0, Music_Routes1_branch_ec04a
+
+Music_Routes1_Ch2::
+	vibrato $12, $2, $2
+	duty $2
+	notetype $c, $9, $2
+	octave 4
+	F# 1
+	G_ 1
+	callchannel Music_Routes1_branch_ec198
+	octave 3
+	A_ 2
+	A_ 2
+	octave 4
+	F# 1
+	G_ 1
+	callchannel Music_Routes1_branch_ec1aa
+	C# 2
+	E_ 2
+	D_ 2
+	F# 2
+	F# 2
+	octave 4
+	D_ 1
+	E_ 1
+	duty $2
+Music_Routes1_branch_ec112::
+	callchannel Music_Routes1_branch_ec188
+	octave 3
+	A_ 2
+	A_ 2
+	octave 4
+	C# 1
+	D_ 1
+	E_ 2
+	E_ 2
+	E_ 2
+	C# 1
+	D_ 1
+	E_ 2
+	E_ 2
+	E_ 2
+	C# 1
+	D_ 1
+	E_ 2
+	E_ 2
+	F# 1
+	E_ 1
+	E_ 1
+	F# 1
+	D_ 2
+	rest 2
+	F# 2
+	D_ 1
+	E_ 1
+	callchannel Music_Routes1_branch_ec188
+	A_ 2
+	A_ 2
+	C# 1
+	D_ 1
+	E_ 2
+	G_ 2
+	F# 2
+	E_ 2
+	D_ 2
+	C# 2
+	octave 3
+	B_ 2
+	A_ 2
+	octave 4
+	B_ 4
+	notetype $8, $9, $2
+	C# 1
 	D_ 1
 	C# 1
+	notetype $c, $9, $2
 	octave 3
 	B_ 1
 	octave 4
 	C# 1
-	octave 3
-	A_ 2
-	A_ 2
-	A_ 6
-	octave 4
-	C# 2
-	C# 6
-	C# 2
-	C# 4
-	octave 3
-	A_ 2
-	octave 4
-	C# 2
-	octave 3
-	B_ 2
-	octave 4
-	C# 4
-	octave 3
-	A_ 2
-	A_ 6
-	octave 4
 	D_ 2
-	D_ 6
+	octave 5
 	D_ 2
-	D_ 6
 	D_ 2
-	D_ 1
-	E_ 1
+	octave 4
+	F# 1
+	G_ 1
+	callchannel Music_Routes1_branch_ec198
+	rest 4
+	F# 1
+	G_ 1
+	callchannel Music_Routes1_branch_ec1aa
+	notetype $8, $9, $2
+	C# 1
 	D_ 1
 	C# 1
-	octave 3
-	B_ 2
-	A_ 2
-	A_ 6
+	notetype $c, $9, $2
 	octave 4
-	C# 2
-	C# 6
-	octave 3
-	A_ 2
-	octave 4
-	E_ 2
-	octave 3
-	A_ 2
-	notetype 12, 10, 2
-	octave 4
-	G_ 4
-	E_ 4
-	F# 2
-	notetype 12, 10, 1
-	octave 3
-	A_ 2
-	A_ 6
-	A_ 2
-	F# 2
-	A_ 4
-	B_ 2
-	octave 4
-	C# 2
-	octave 3
-	B_ 4
-	A_ 2
-	F# 2
-	A_ 4
-	G_ 2
-	E_ 2
-	C# 4
-	A_ 2
-	octave 4
+	B_ 1
+	octave 5
+	C# 1
 	D_ 2
-	octave 3
-	A_ 4
-	B_ 2
-	G_ 2
-	B_ 4
-	octave 4
-	D_ 2
-	E_ 2
-	C# 2
-	D_ 2
-	octave 3
-	A_ 2
-	A_ 2
-	loopchannel 0, Music_Routes1_branch_9be9
-	endchannel
-
-
-Music_Routes1_Ch2::
-	duty 2
-
-Music_Routes1_branch_9c53::
-	notetype 12, 13, 1
-	callchannel Music_Routes1_branch_9c65
-	callchannel Music_Routes1_branch_9c78
-	callchannel Music_Routes1_branch_9c65
-	callchannel Music_Routes1_branch_9c8d
-	loopchannel 0, Music_Routes1_branch_9c53
-
-Music_Routes1_branch_9c65::
+	F# 2
+	F# 2
 	octave 4
 	D_ 1
 	E_ 1
+	loopchannel 0, Music_Routes1_branch_ec112
+
+Music_Routes1_branch_ec188::
 	F# 2
 	F# 2
 	F# 2
@@ -135,62 +304,10 @@ Music_Routes1_branch_9c65::
 	F# 2
 	G_ 3
 	F# 1
-	E_ 6
+	E_ 2
 	endchannel
 
-Music_Routes1_branch_9c78::
-	C# 1
-	D_ 1
-	E_ 2
-	E_ 2
-	E_ 2
-	C# 1
-	D_ 1
-	E_ 2
-	E_ 2
-	E_ 2
-	C# 1
-	D_ 1
-	E_ 2
-	E_ 2
-	F# 1
-	E_ 1
-	E_ 1
-	F# 1
-	D_ 4
-	F# 2
-	endchannel
-
-Music_Routes1_branch_9c8d::
-	C# 1
-	D_ 1
-	E_ 2
-	G_ 2
-	F# 2
-	E_ 2
-	D_ 2
-	C# 2
-	octave 3
-	B_ 2
-	octave 4
-	C# 2
-	notetype 12, 13, 2
-	B_ 4
-	notetype 6, 13, 1
-	octave 3
-	B_ 1
-	octave 4
-	C# 1
-	notetype 12, 13, 1
-	octave 3
-	B_ 1
-	A_ 1
-	octave 4
-	C# 1
-	D_ 6
-	notetype 12, 13, 2
-	F# 1
-	G_ 1
+Music_Routes1_branch_ec198::
 	A_ 2
 	A_ 2
 	F# 2
@@ -201,19 +318,20 @@ Music_Routes1_branch_9c8d::
 	octave 4
 	B_ 2
 	octave 5
-	C# 2
+	D_ 2
 	octave 4
 	A_ 2
 	F# 2
 	D_ 3
 	F# 1
-	E_ 6
-	F# 1
-	G_ 1
+	E_ 2
+	endchannel
+
+Music_Routes1_branch_ec1aa::
 	A_ 2
 	A_ 2
 	F# 2
-	A_ 2
+	D_ 2
 	octave 5
 	D_ 2
 	C# 2
@@ -223,92 +341,276 @@ Music_Routes1_branch_9c8d::
 	A_ 2
 	octave 5
 	D_ 2
-	C# 2
-	E_ 2
-	D_ 2
-	notetype 12, 13, 1
-	octave 4
-	D_ 2
-	D_ 2
 	endchannel
-	endchannel
-
 
 Music_Routes1_Ch3::
-	vibrato 8, 2, 5
-	notetype 12, 1, 3
-
-Music_Routes1_branch_9cdd::
+	vibrato $10, $2, $3
+	notetype $c, $2, $5
 	rest 2
+	octave 3
+	D_ 1
+	rest 1
+	A_ 1
+	rest 1
 	octave 4
+	F# 1
+	rest 3
+	octave 3
+	D_ 1
+	rest 1
+	B_ 1
+	rest 1
+	octave 4
+	G_ 1
+	rest 1
+	octave 3
+	B_ 1
+	rest 1
+	D_ 1
+	rest 1
+	A_ 1
+	rest 1
+	octave 4
+	D_ 1
+	rest 3
+	octave 3
+	C# 1
+	rest 1
+	A_ 1
+	rest 1
+	octave 4
+	C# 1
+	rest 1
+	C# 1
+	octave 3
+	A_ 1
+	D_ 1
+	octave 2
+	A_ 1
+	octave 3
+	D_ 1
+	F# 1
+	octave 4
+	D_ 1
+	octave 3
+	A_ 1
+	F# 1
+	A_ 1
+	G_ 1
+	D_ 1
+	G_ 1
+	B_ 1
+	octave 4
+	G_ 1
+	D_ 1
+	octave 3
+	B_ 1
+	G_ 1
+	octave 2
+	A_ 1
+	octave 3
+	C# 1
+	E_ 1
+	G_ 1
+	E_ 1
+	A_ 1
+	octave 4
+	C# 1
+	G_ 1
+	F# 2
+	A_ 1
+	rest 1
+	A_ 1
+	rest 3
+Music_Routes1_branch_ec20c::
+	octave 3
+	D_ 1
+	rest 1
+	octave 4
+	D_ 2
+	octave 3
+	C# 1
+	rest 1
+	octave 4
+	C# 2
+	octave 2
+	B_ 1
+	rest 1
+	octave 3
+	B_ 2
+	octave 2
+	A_ 1
+	rest 1
+	octave 3
+	A_ 2
+	D_ 1
+	rest 1
+	octave 4
+	D_ 2
+	octave 2
+	A_ 1
+	rest 1
+	octave 3
+	A_ 2
+	octave 2
+	B_ 1
+	rest 1
+	octave 3
+	B_ 2
+	octave 2
+	A_ 1
+	rest 1
+	octave 3
+	A_ 2
+	C# 1
+	rest 1
+	octave 4
+	C# 2
+	octave 2
+	A_ 1
+	rest 1
+	octave 3
+	A_ 2
+	octave 2
+	B_ 1
+	rest 1
+	octave 3
+	B_ 2
+	C_ 1
+	rest 1
+	octave 4
+	C_ 2
+	octave 3
+	C# 1
+	rest 1
+	octave 4
+	C# 2
+	octave 2
+	A_ 1
+	rest 1
+	octave 3
+	A_ 2
+	octave 4
+	D_ 2
+	C# 1
+	octave 3
+	B_ 1
+	A_ 2
+	G_ 1
+	E_ 1
+	octave 2
+	D_ 1
+	rest 1
+	octave 3
+	D_ 2
+	octave 2
+	E_ 1
+	rest 1
+	octave 3
+	E_ 2
+	octave 2
+	F# 1
+	rest 1
+	octave 3
+	F# 2
+	octave 2
+	A_ 1
+	rest 1
+	octave 3
+	A_ 2
+	octave 4
+	D_ 2
+	octave 3
+	A_ 2
+	octave 4
+	D_ 2
+	F# 2
+	E_ 2
+	octave 3
+	E_ 1
+	rest 1
+	E_ 1
+	rest 1
+	octave 4
+	E_ 2
+	octave 3
+	C# 1
+	rest 1
+	octave 4
+	C# 2
+	octave 2
+	B_ 1
+	rest 1
+	octave 3
+	B_ 2
+	octave 2
+	A_ 1
+	rest 1
+	octave 3
+	A_ 2
+	octave 2
+	G_ 1
+	rest 1
+	octave 3
+	G_ 1
+	B_ 1
+	octave 4
+	C# 2
+	E_ 1
+	C# 1
+	octave 3
+	A_ 1
+	G_ 1
+	E_ 1
+	C# 1
+	D_ 2
+	octave 4
+	F# 1
+	rest 1
+	F# 1
+	rest 3
+	octave 3
 	D_ 4
-	C# 4
+	F# 4
+	G_ 2
+	A_ 2
+	B_ 3
+	G_ 1
+	F# 2
+	D_ 2
+	octave 2
+	A_ 3
 	octave 3
-	B_ 4
-	A_ 4
-	octave 4
-	D_ 4
+	D_ 1
+	A_ 1
+	B_ 1
+	A_ 1
+	B_ 1
+	A_ 3
+	G_ 1
+	F# 2
+	D_ 2
+	octave 2
+	A_ 2
 	octave 3
-	A_ 4
-	B_ 4
-	A_ 4
+	D_ 2
+	G_ 2
+	F# 2
+	E_ 3
+	C# 1
+	D_ 2
+	F# 2
+	A_ 2
 	octave 4
-	C# 4
-	octave 3
-	A_ 4
-	B_ 4
-	octave 4
-	C_ 4
-	C# 4
-	octave 3
-	A_ 4
-	octave 4
-	D_ 4
-	octave 3
-	A_ 4
-	octave 4
-	D_ 4
-	C# 4
-	octave 3
-	B_ 4
-	A_ 4
-	octave 4
-	D_ 4
-	octave 3
-	A_ 4
-	B_ 4
-	A_ 4
-	octave 4
-	C# 4
-	octave 3
-	B_ 4
-	A_ 4
-	B_ 4
-	octave 4
-	C# 4
-	octave 3
-	A_ 4
-	octave 4
-	D_ 4
-	octave 3
-	A_ 4
-	octave 4
-	D_ 8
-	octave 3
-	G_ 8
-	A_ 8
-	octave 4
-	C# 8
-	D_ 8
-	octave 3
-	G_ 8
-	A_ 8
-	octave 4
-	D_ 6
-	loopchannel 0, Music_Routes1_branch_9cdd
-	endchannel
-
-
+	C# 2
+	D_ 2
+	octave 5
+	A_ 1
+	rest 1
+	A_ 1
+	rest 3
+	loopchannel 0, Music_Routes1_branch_ec20c
+	
 Music_Routes1_Ch4::
 	dspeed 12
 	rest 4
